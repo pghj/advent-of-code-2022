256984c33689d9c1f0d5123a36ef7b534f8476ba
pghj <pghjvanblokland@gmail.com>
2022-12-20 16:41:38 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
-# advent-of-code-2022
+# Advent of Code 2022
 
-Welcome to the Advent of Code[^aoc] Kotlin project created by [pghj][github] using the [Advent of Code Kotlin Template][template] delivered by JetBrains.
+[My][github] solutions to the Advent of Code[^aoc] 2022.
 
-In this repository, pghj is about to provide solutions for the puzzles using [Kotlin][kotlin] language.
+All solutions are written in [Kotlin][kotlin] without using external libraries.
 
-If you're stuck with Kotlin-specific questions or anything related to this template, check out the following resources:
+## Execution times
 
-- [Kotlin docs][docs]
-- [Kotlin Slack][slack]
-- Template [issue tracker][issues]
-
+| Day  | Execution time |
+|------|---------------:|
+| 01.a |          33 ms |
+| 01.b |          10 ms |
+| 02.a |           2 ms |
+| 02.b |           2 ms |
+| 03.a |          16 ms |
+| 03.b |           6 ms |
+| 04.a |           9 ms |
+| 04.b |           5 ms |
+| 05.a |           5 ms |
+| 05.b |           4 ms |
+| 06.a |           1 ms |
+| 06.b |           1 ms |
+| 07.a |           7 ms |
+| 07.b |           4 ms |
+| 08.a |          31 ms |
+| 08.b |          32 ms |
+| 09.a |          12 ms |
+| 09.b |          39 ms |
+| 10.a |           0 ms |
+| 10.b |           9 ms |
+| 11.a |          14 ms |
+| 11.b |         467 ms |
+| 12.a |          19 ms |
+| 12.b |          11 ms |
+| 13.a |           5 ms |
+| 13.b |           8 ms |
+| 14.a |          39 ms |
+| 14.b |         441 ms |
+| 15.a |           6 ms |
+| 15.b |        2199 ms |
+| 16.a |          94 ms |
+| 16.b |        1277 ms |
+| 17.a |          28 ms |
+| 17.b |          51 ms |
+| 18.a |          16 ms |
+| 18.b |         510 ms |
+| 19.a |         263 ms |
+| 19.b |         394 ms |
+| 20.a |         154 ms |
+| 20.b |         378 ms |
 
 [^aoc]:
     [Advent of Code][aoc] – An annual event of Christmas-oriented programming challenges started December 2015.
@@ -17,9 +55,5 @@ If you're stuck with Kotlin-specific questions or anything related to this templ
     You can solve the puzzle and provide an answer using the language of your choice.
 
 [aoc]: https://adventofcode.com
-[docs]: https://kotlinlang.org/docs/home.html
 [github]: https://github.com/pghj
-[issues]: https://github.com/kotlin-hands-on/advent-of-code-kotlin-template/issues
 [kotlin]: https://kotlinlang.org
-[slack]: https://surveys.jetbrains.com/s3/kotlin-slack-sign-up
-[template]: https://github.com/kotlin-hands-on/advent-of-code-kotlin-template
--- src/all.kt
+++ src/all.kt
@@ -1,65 +1,76 @@
 fun main() {
-    var t0 = System.currentTimeMillis()
 
-    fun d(s: String) {
+    class Result(
+        val name: String,
+        val time: Long
+    )
+
+    val results = ArrayList<Result>()
+
+    fun d(s: String, job: ()->Unit) {
         println("\nDay $s\n========\n")
-        t0 = System.currentTimeMillis()
-    }
-    fun e() {
+        val t0 = System.currentTimeMillis()
+        job()
         val d = System.currentTimeMillis() - t0
         println("\ntime: ${d}ms\n")
+        results.add(Result(s, d))
     }
+    
+    d("01.a") { day01.a.main() }
+    d("01.b") { day01.b.main() }
+    d("02.a") { day02.a.main() }
+    d("02.b") { day02.b.main() }
+    d("03.a") { day03.a.main() }
+    d("03.b") { day03.b.main() }
+    d("04.a") { day04.a.main() }
+    d("04.b") { day04.b.main() }
+    d("05.a") { day05.a.main() }
+    d("05.b") { day05.b.main() }
+    d("06.a") { day06.a.main() }
+    d("06.b") { day06.b.main() }
+    d("07.a") { day07.a.main() }
+    d("07.b") { day07.b.main() }
+    d("08.a") { day08.a.main() }
+    d("08.b") { day08.b.main() }
+    d("09.a") { day09.a.main() }
+    d("09.b") { day09.b.main() }
+    d("10.a") { day10.a.main() }
+    d("10.b") { day10.b.main() }
+    d("11.a") { day11.a.main() }
+    d("11.b") { day11.b.main() }
+    d("12.a") { day12.a.main() }
+    d("12.b") { day12.b.main() }
+    d("13.a") { day13.a.main() }
+    d("13.b") { day13.b.main() }
+    d("14.a") { day14.a.main() }
+    d("14.b") { day14.b.main() }
+    d("15.a") { day15.a.main() }
+    d("15.b") { day15.b.main() }
+    d("16.a") { day16.a.main() }
+    d("16.b") { day16.b.main() }
+    d("17.a") { day17.a.main() }
+    d("17.b") { day17.b.main() }
+    d("18.a") { day18.a.main() }
+    d("18.b") { day18.b.main() }
+    d("19.a") { day19.a.main() }
+    d("19.b") { day19.b.main() }
+    d("20.a") { day20.a.main() }
+    d("20.b") { day20.b.main() }
+//    d("21.a") { day21.a.main() }
+//    d("21.b") { day21.b.main() }
+//    d("22.a") { day22.a.main() }
+//    d("22.b") { day22.b.main() }
+//    d("23.a") { day23.a.main() }
+//    d("23.b") { day23.b.main() }
+//    d("24.a") { day24.a.main() }
+//    d("24.b") { day24.b.main() }
+//    d("25.a") { day25.a.main() }
+//    d("25.b") { day25.b.main() }
 
-    d("01.a"); day01.a.main(); e()
-    d("01.b"); day01.b.main(); e()
-    d("02.a"); day02.a.main(); e()
-    d("02.b"); day02.b.main(); e()
-    d("03.a"); day03.a.main(); e()
-    d("03.b"); day03.b.main(); e()
-    d("04.a"); day04.a.main(); e()
-    d("04.b"); day04.b.main(); e()
-    d("05.a"); day05.a.main(); e()
-    d("05.b"); day05.b.main(); e()
-    d("06.a"); day06.a.main(); e()
-    d("06.b"); day06.b.main(); e()
-    d("07.a"); day07.a.main(); e()
-    d("07.b"); day07.b.main(); e()
-    d("08.a"); day08.a.main(); e()
-    d("08.b"); day08.b.main(); e()
-    d("09.a"); day09.a.main(); e()
-    d("09.b"); day09.b.main(); e()
-    d("10.a"); day10.a.main(); e()
-    d("10.b"); day10.b.main(); e()
-    d("11.a"); day11.a.main(); e()
-    d("11.b"); day11.b.main(); e()
-    d("12.a"); day12.a.main(); e()
-    d("12.b"); day12.b.main(); e()
-    d("13.a"); day13.a.main(); e()
-    d("13.b"); day13.b.main(); e()
-    d("14.a"); day14.a.main(); e()
-    d("14.b"); day14.b.main(); e()
-    d("15.a"); day15.a.main(); e()
-    d("15.b"); day15.b.main(); e()
-    d("16.a"); day16.a.main(); e()
-    d("16.b"); day16.b.main(); e()
-    d("17.a"); day17.a.main(); e()
-    d("17.b"); day17.b.main(); e()
-    d("18.a"); day18.a.main(); e()
-    d("18.b"); day18.b.main(); e()
-    d("19.a"); day19.a.main(); e()
-    d("19.b"); day19.b.main(); e()
-    d("20.a"); day20.a.main(); e()
-    d("20.b"); day20.b.main(); e()
-//    d("21.a"); day21.a.main(); e()
-//    d("21.b"); day21.b.main(); e()
-//    d("22.a"); day22.a.main(); e()
-//    d("22.b"); day22.b.main(); e()
-//    d("23.a"); day23.a.main(); e()
-//    d("23.b"); day23.b.main(); e()
-//    d("24.a"); day24.a.main(); e()
-//    d("24.b"); day24.b.main(); e()
-//    d("25.a"); day25.a.main(); e()
-//    d("25.b"); day25.b.main(); e()
-
+    println("| Day  | Execution time |")
+    println("|------|---------------:|")
+    for (r in results) {
+        println(String.format("| %s | %11d ms |", r.name, r.time))
+    }
 
 }
